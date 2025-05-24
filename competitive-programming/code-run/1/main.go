d765755d07d54559e935affbf172d6796e57449f
DimaChekashov <foxdima99@gmail.com>
2025-05-24 23:14:34 +0300

    add first code run problem

---
+++ competitive-programming/code-run/1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sort"
+)
+
+func main() {
+	var arr = make([]int, 3)
+
+	for i := 0; i < len(arr); i++ {
+		_, err := fmt.Scan(&arr[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+
+	sort.Ints(arr)
+
+	fmt.Println(arr[1])
+}
