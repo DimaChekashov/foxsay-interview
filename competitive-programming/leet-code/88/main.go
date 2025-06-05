0372cb7745708a7c5df5dc21eb4dba68f9773e26
DimaChekashov <foxdima99@gmail.com>
2025-06-05 19:30:24 +0300

    add leetcode problem 88

---
+++ competitive-programming/leet-code/88/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
+
+	merge(nums1, m, nums2, n)
+	fmt.Println(nums1)
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	for n != 0 {
+		if m != 0 && nums1[m-1] > nums2[n-1] {
+			nums1[m+n-1] = nums1[m-1]
+			m--
+		} else {
+			nums1[m+n-1] = nums2[n-1]
+			n--
+		}
+	}
+}
