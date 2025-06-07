a48412e6b7dcfebd090901729573ae5741684004
DimaChekashov <foxdima99@gmail.com>
2025-06-07 09:50:13 +0300

    add leetcode problem 119

---
+++ competitive-programming/leet-code/119/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(getRow(3))
+}
+
+func getRow(rowIndex int) []int {
+	res := []int{1}
+	prev := 1
+
+	for k := 1; k <= rowIndex; k++ {
+		next_val := prev * (rowIndex - k + 1) / k
+		res = append(res, next_val)
+		prev = next_val
+	}
+
+	return res
+}
