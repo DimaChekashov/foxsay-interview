08cffd0da9d0c6bf4573e8aa08d35f455da6920e
DimaChekashov <foxdima99@gmail.com>
2025-07-15 22:20:35 +0300

    add two sum

---
+++ competitive-programming/leet-code/1/two_sum.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+        
+        return []
+
+# Пример использования
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([2, 7, 11, 15], 9))
+    print(solution.twoSum([3, 2, 4], 6))
+    print(solution.twoSum([3, 3], 6))
