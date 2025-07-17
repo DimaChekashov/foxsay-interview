976ede9bfac0e99ce7a3bd93a2ea08e0b0712366
DimaChekashov <foxdima99@gmail.com>
2025-07-17 22:32:47 +0300

    add romanToInt

---
+++ competitive-programming/leet-code/13/roman_to_int.py
@@ -0,0 +1,27 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        m = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        ans = 0
+
+        for i in range(len(s)):
+            if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+                ans -= m[s[i]]
+            else:
+                ans += m[s[i]]
+
+        return ans
+    
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.romanToInt("III"))
+    print(solution.romanToInt("LVIII"))
+    print(solution.romanToInt("MCMXCIV"))
