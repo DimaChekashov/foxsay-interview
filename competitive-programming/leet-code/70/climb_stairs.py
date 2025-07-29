5ab5cf3d125a16890d0669bbcc7fca88ca0d5922
DimaChekashov <foxdima99@gmail.com>
2025-07-29 17:20:26 +0300

    add climb stairs

---
+++ competitive-programming/leet-code/70/climb_stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        return self.helper(n, memo)
+    
+    def helper(self, n: int, memo: dict[int, int]) -> int:
+        if n == 0 or n == 1:
+            return 1
+        if n not in memo:
+            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
+        return memo[n]
+    
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.climbStairs(2))
+    print(solution.climbStairs(3))
