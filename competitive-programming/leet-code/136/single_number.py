837385d817ee4de1bd3a96d885d52666cb3c714e
DimaChekashov <foxdima99@gmail.com>
2025-08-05 19:06:30 +0300

    add single number

---
+++ competitive-programming/leet-code/136/single_number.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        
+        return xor
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.singleNumber([2,2,1]))
+    print(solution.singleNumber([4,1,2,1,2]))
+    print(solution.singleNumber([1]))
