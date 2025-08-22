be6acb180c1592919cee27c04adae9a8cd891b96
DimaChekashov <foxdima99@gmail.com>
2025-08-22 19:54:26 +0300

    add intersect

---
+++ competitive-programming/leet-code/350/intersect.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+
+        result = []
+
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+
+        return result
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.intersect([1,2,2,1], [2,2]))
+    print(solution.intersect([4,9,5], [9,4,9,8,4]))
