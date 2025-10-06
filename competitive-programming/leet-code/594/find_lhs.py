61c2ee48a4216f3edec42e1546a2a1754c2c008c
DimaChekashov <foxdima99@gmail.com>
2025-10-06 20:17:31 +0300

    add findLHS

---
+++ competitive-programming/leet-code/594/find_lhs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findLHS(self, nums):
+        nums.sort()
+        j = 0
+        maxLength = 0
+
+        for i in range(len(nums)):
+            while nums[i] - nums[j] > 1:
+                j += 1
+            if nums[i] - nums[j] == 1:
+                maxLength = max(maxLength, i - j + 1)
+        return maxLength
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.findLHS([1,3,2,2,5,2,3,7]))
+    print(solution.findLHS([1,2,3,4]))
+    print(solution.findLHS([1,1,1,1]))
