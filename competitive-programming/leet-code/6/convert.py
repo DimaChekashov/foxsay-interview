bf5cf8e9939eb4b0560715cbd06182ede2a4abaf
DimaChekashov <foxdima99@gmail.com>
2025-11-01 17:08:44 +0300

    add convert

---
+++ competitive-programming/leet-code/6/convert.py
@@ -0,0 +1,26 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        idx, d = 0, 1
+        rows = [[] for _ in range(numRows)]
+
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == numRows - 1:
+                d = -1
+            idx += d
+
+        for i in range(numRows):
+            rows[i] = ''.join(rows[i])
+
+        return ''.join(rows)   
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.convert("PAYPALISHIRING", 3))
+    print(solution.convert("PAYPALISHIRING", 4))
+    print(solution.convert("A", 1))
