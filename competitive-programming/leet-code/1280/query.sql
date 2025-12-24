4974cad9aa1db3ef001053eb2b2bd447f070bf66
DimaChekashov <foxdima99@gmail.com>
2025-12-24 20:26:53 +0300

    Add Students and Examinations

---
+++ competitive-programming/leet-code/1280/query.sql
@@ -0,0 +1,11 @@
+SELECT 
+    stu.student_id, 
+    stu.student_name, 
+    sub.subject_name, 
+    count(exam.student_id) AS attended_exams
+FROM Students stu
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations exam
+    ON exam.student_id = stu.student_id AND sub.subject_name = exam.subject_name
+GROUP BY stu.student_id, stu.student_name, sub.subject_name
+ORDER BY stu.student_id, stu.student_name, sub.subject_name;
