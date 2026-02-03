520fd7323dc8a766f1f349cfc57ce8e13be0d630
DimaChekashov <foxdima99@gmail.com>
2026-02-03 18:29:58 +0300

    Add sortedListToBST

---
+++ competitive-programming/leet-code/109/sortedListToBST.js
@@ -0,0 +1,16 @@
+var sortedListToBST = function (head) {
+  let curr = head,
+    count = 0;
+  while (curr) ((curr = curr.next), count++);
+  const treeify = (i, j) => {
+    if (j < i) return null;
+    let mid = (i + j) >> 1,
+      node = new TreeNode();
+    node.left = treeify(i, mid - 1);
+    ((node.val = curr.val), (curr = curr.next));
+    node.right = treeify(mid + 1, j);
+    return node;
+  };
+  curr = head;
+  return treeify(1, count);
+};
