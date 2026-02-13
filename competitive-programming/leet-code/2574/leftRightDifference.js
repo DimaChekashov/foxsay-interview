c88a2493f6cd984843d0fa5b003512c75c885a8a
DimaChekashov <foxdima99@gmail.com>
2026-02-13 20:25:50 +0300

    Add Left and Right Sum Differences

---
+++ competitive-programming/leet-code/2574/leftRightDifference.js
@@ -0,0 +1,17 @@
+var leftRightDifference = function (nums) {
+  let result = [];
+
+  let left = 0;
+  let right = 0;
+
+  for (var i = 0; i < nums.length; i++) {
+    right += nums[i];
+  }
+
+  for (var i = 0; i < nums.length; i++) {
+    right -= nums[i];
+    result.push(Math.abs(left - right));
+    left += nums[i];
+  }
+  return result;
+};
