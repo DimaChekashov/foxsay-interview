7b82d8af6889d4a9672508d3d927216e18e7b933
DimaChekashov <foxdima99@gmail.com>
2026-02-21 19:49:01 +0300

    Add Largest Rectangle in Histogram

---
+++ competitive-programming/leet-code/84/largestRectangleArea.js
@@ -0,0 +1,24 @@
+var largestRectangleArea = function (heights) {
+  const stack = [-1];
+  let max_area = 0;
+
+  for (let i = 0; i < heights.length; i++) {
+    while (
+      stack[stack.length - 1] !== -1 &&
+      heights[i] <= heights[stack[stack.length - 1]]
+    ) {
+      const height = heights[stack.pop()];
+      const width = i - stack[stack.length - 1] - 1;
+      max_area = Math.max(max_area, height * width);
+    }
+    stack.push(i);
+  }
+
+  while (stack[stack.length - 1] !== -1) {
+    const height = heights[stack.pop()];
+    const width = heights.length - stack[stack.length - 1] - 1;
+    max_area = Math.max(max_area, height * width);
+  }
+
+  return max_area;
+};
