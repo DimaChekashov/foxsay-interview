fc41374ec4171e2fdcc8f899c726bb2b16cd1a41
Dima Chekashov <foxdima99@gmail.com>
2026-05-13 19:06:34 +0300

    Add Remove Linked List Elements

---
+++ competitive-programming/leet-code/203/removeElements.js
@@ -0,0 +1,13 @@
+var removeElements = function (head, val) {
+    let ans = new ListNode(0, head);
+    let dummy = ans;
+
+    while (dummy) {
+        while (dummy.next && dummy.next.val === val) {
+            dummy.next = dummy.next.next;
+        }
+        dummy = dummy.next;
+    }
+
+    return ans.next;
+};
