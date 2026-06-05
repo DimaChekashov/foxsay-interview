e7d843d44e4779ac8239fe2e74c4592481e2c501
DimaChekashov <foxdima99@gmail.com>
2026-06-05 18:59:47 +0300

    Add Diagonal Traverse

---
+++ competitive-programming/leet-code/498/findDiagonalOrder.js
@@ -0,0 +1,21 @@
+var findDiagonalOrder = function (mat) {
+  const m = mat.length,
+    n = mat[0].length;
+  const map = new Map();
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      const key = i + j;
+      if (!map.has(key)) map.set(key, []);
+      map.get(key).push(mat[i][j]);
+    }
+  }
+
+  const res = [];
+  for (let k = 0; k < m + n - 1; k++) {
+    const list = map.get(k);
+    if (k % 2 === 0) list.reverse();
+    res.push(...list);
+  }
+  return res;
+};
