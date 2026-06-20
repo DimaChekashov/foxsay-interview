7a314249a673f85428226a87407e76fecc988231
Dima Chekashov <foxdima99@gmail.com>
2026-06-20 16:16:59 +0300

    Add Range Addition II

---
+++ competitive-programming/leet-code/598/maxCount.js
@@ -0,0 +1,9 @@
+var maxCount = function (m, n, ops) {
+    if (ops.length === 0) return m * n;
+    let minA = m, minB = n;
+    for (let [a, b] of ops) {
+        minA = Math.min(minA, a);
+        minB = Math.min(minB, b);
+    }
+    return minA * minB;
+};
