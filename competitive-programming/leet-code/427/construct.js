f8e91c8b835d5aff6efb5748807e288be3637b19
Dima Chekashov <foxdima99@gmail.com>
2026-07-09 18:18:02 +0300

    Add Construct Quad Tree

---
+++ competitive-programming/leet-code/427/construct.js
@@ -0,0 +1,29 @@
+var construct = function (grid) {
+  const build = (x, y, size) => {
+    if (size === 1) {
+      return new Node(grid[x][y] === 1, true);
+    }
+
+    const half = size >> 1;
+    const topLeft = build(x, y, half);
+    const topRight = build(x, y + half, half);
+    const bottomLeft = build(x + half, y, half);
+    const bottomRight = build(x + half, y + half, half);
+
+    if (
+      topLeft.isLeaf &&
+      topRight.isLeaf &&
+      bottomLeft.isLeaf &&
+      bottomRight.isLeaf &&
+      topLeft.val === topRight.val &&
+      topLeft.val === bottomLeft.val &&
+      topLeft.val === bottomRight.val
+    ) {
+      return new Node(topLeft.val, true);
+    }
+
+    return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+  };
+
+  return build(0, 0, grid.length);
+};
