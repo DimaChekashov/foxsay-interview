8d15caf1080cee08cc2741603ff4f56c403cfc93
Dima Chekashov <foxdima99@gmail.com>
2026-07-14 18:04:10 +0300

    Add Repeated String Match

---
+++ competitive-programming/leet-code/686/repeatedStringMatch.js
@@ -0,0 +1,8 @@
+var repeatedStringMatch = function (a, b) {
+    let times = Math.ceil(b.length / a.length);
+    let repeated = a.repeat(times);
+
+    if (repeated.includes(b)) return times;
+    if ((repeated + a).includes(b)) return times + 1;
+    return -1;
+};
