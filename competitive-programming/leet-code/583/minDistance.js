ffe4f9af93904f9687dd6057699eb9632a96765a
Dima Chekashov <foxdima99@gmail.com>
2026-07-16 17:03:27 +0300

    Add Delete Operation for Two Strings

---
+++ competitive-programming/leet-code/583/minDistance.js
@@ -0,0 +1,15 @@
+var minDistance = function (word1, word2) {
+    const n = word1.length, m = word2.length;
+    const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return n + m - 2 * dp[n][m];
+};
