ec2ea997198b5f20979c7621fe4473b043a8e212
Dima Chekashov <foxdima99@gmail.com>
2026-07-19 18:29:40 +0300

    Add lowest_common_ancestor

---
+++ competitive-programming/leet-code/235/lowest_common_ancestor.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowest_common_ancestor(self, root: Optional[TreeNode], p: TreeNode, q: TreeNode) -> Optional[TreeNode]:
+        current = root
+
+        while current:
+            if p.val > current.val and q.val > current.val:
+                current = current.right
+            elif p.val < current.val and q.val < current.val:
+                current = current.left
+            else:
+                return current
+
+        return None
