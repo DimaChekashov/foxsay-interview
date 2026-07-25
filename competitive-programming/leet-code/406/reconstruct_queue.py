0fe50cd612d482ded11350fcbe6332383a927913
Dima Chekashov <foxdima99@gmail.com>
2026-07-25 18:26:20 +0300

    Add reconstruct_queue

---
+++ competitive-programming/leet-code/406/reconstruct_queue.py
@@ -0,0 +1,10 @@
+import bisect
+
+def reconstruct_queue(people: List[List[int]]) -> List[List[int]]:
+    people.sort(key=lambda x: (-x[0], x[1]))
+    result = []
+
+    for person in people:
+        result.insert(person[1], person)
+
+    return result
