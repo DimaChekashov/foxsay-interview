d85a9ea55f98e13bfa99ce1b53df911c5636642f
Dima Chekashov <foxdima99@gmail.com>
2026-08-01 14:08:41 +0300

    Add predict_the_winner

---
+++ competitive-programming/leet-code/486/predict_the_winner.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def predict_the_winner(nums: List[int]) -> bool:
+    def helper(left: int, right: int) -> int:
+        if left == right:
+            return nums[left]
+
+        pick_left = nums[left] - helper(left + 1, right)
+        pick_right = nums[right] - helper(left, right - 1)
+
+        return max(pick_left, pick_right)
+
+    return helper(0, len(nums) - 1) >= 0
