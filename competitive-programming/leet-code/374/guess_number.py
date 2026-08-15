ccc03ea2a5af82614f8dd3aacdb32d12a89705ea
Dima Chekashov <foxdima99@gmail.com>
2026-08-15 22:21:35 +0300

    Add guess_number

---
+++ competitive-programming/leet-code/374/guess_number.py
@@ -0,0 +1,15 @@
+def guess_number(n: int) -> int:
+    left, right = 1, n
+
+    while left <= right:
+        mid = (left + right) // 2
+        res = guess(mid)
+
+        if res == 0:
+            return mid
+        elif res == -1:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1
