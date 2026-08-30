169a354d890f644110f0a9336846df8f40250732
Dima Chekashov <foxdima99@gmail.com>
2026-08-30 16:13:54 +0300

    Add recover_tree

---
+++ competitive-programming/leet-code/99/recover_tree.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def recover_tree(root: Optional[TreeNode]) -> None:
+    first = None
+    second = None
+    prev = None
+
+    def inorder(node: Optional[TreeNode]) -> None:
+        nonlocal first, second, prev
+
+        if not node:
+            return
+
+        inorder(node.left)
+
+        if prev and prev.val > node.val:
+            if not first:
+                first = prev
+            second = node
+
+        prev = node
+
+        inorder(node.right)
+
+    inorder(root)
+
+    if first and second:
+        first.val, second.val = second.val, first.val
