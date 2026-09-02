8d413638674fff309c70fd9d4ff809df6f701f5a
Dima Chekashov <foxdima99@gmail.com>
2026-09-02 17:40:20 +0300

    Add Array Nesting

---
+++ competitive-programming/leet-code/565/arrayNesting.js
@@ -0,0 +1,10 @@
+var arrayNesting = function (nums) {
+    const visited = new Set();
+    const dfs = (num, len) => {
+        if (visited.has(num)) return len;
+        visited.add(num);
+        len++;
+        return dfs(nums[num], len);
+    }
+    return nums.reduce((len, curr) => visited.has(curr) ? len : Math.max(dfs(curr, 0), len), 0);
+};
