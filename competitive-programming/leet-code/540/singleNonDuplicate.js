39125d9fd42d56090a330de94fd1935fdb31c4d7
Dima Chekashov <foxdima99@gmail.com>
2026-09-14 18:07:24 +0300

    Add Single Element in a Sorted Array

---
+++ competitive-programming/leet-code/540/singleNonDuplicate.js
@@ -0,0 +1,20 @@
+var singleNonDuplicate = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
+
+        if (mid % 2 === 1) {
+            mid--;
+        }
+
+        if (nums[mid] === nums[mid + 1]) {
+            left = mid + 2;
+        } else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+};
