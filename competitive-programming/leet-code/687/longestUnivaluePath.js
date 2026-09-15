efe5ada5f9059f9b430fb426cb9afca7279a005b
Dima Chekashov <foxdima99@gmail.com>
2026-09-15 16:25:06 +0300

    Add Longest Univalue Path

---
+++ competitive-programming/leet-code/687/longestUnivaluePath.js
@@ -0,0 +1,26 @@
+var longestUnivaluePath = function (root) {
+    let max = 0;
+
+    const dfs = (node) => {
+        if (!node) return 0;
+
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+
+        let leftPath = 0, rightPath = 0;
+
+        if (node.left && node.left.val === node.val) {
+            leftPath = left + 1;
+        }
+        if (node.right && node.right.val === node.val) {
+            rightPath = right + 1;
+        }
+
+        max = Math.max(max, leftPath + rightPath);
+
+        return Math.max(leftPath, rightPath);
+    }
+
+    dfs(root);
+    return max;
+};
