73d4f12d0cc323c7a85853f336a4ed4dbbc3336a
Dima Chekashov <foxdima99@gmail.com>
2026-09-25 17:41:26 +0300

    Add Reach a Number

---
+++ competitive-programming/leet-code/754/reachNumber.js
@@ -0,0 +1,14 @@
+var reachNumber = function (target) {
+    target = Math.abs(target);
+    let step = 0;
+    let sum = 0;
+    while (sum < target) {
+        step++;
+        sum += step;
+    }
+    while ((sum - target) % 2 != 0) {
+        step++;
+        sum += step;
+    }
+    return step;
+};
